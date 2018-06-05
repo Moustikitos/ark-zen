b3f8904adf35c3306d29873447c11565050b8c96
Moustikitos <bruno.thoorens@free.fr>
2018-06-05 21:16:36 +0200

    Update wsgi.py
    Use relative path

--- zen/app/wsgi.py
+++ zen/app/wsgi.py
@@ -1,5 +1,7 @@
+import os
 import sys
-sys.path.append("/home/dark/zen")
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 print(sys.path)
 from app import app
 
