f0f9476e57aac8cb0c9d7fad08ed0f38a4e42b69
Moustikitos <moustikitos@gmail.com>
2021-01-16 14:41:22 +0100

    flake8 compliancy

--- zen/app/core.py
+++ zen/app/core.py
@@ -11,23 +11,23 @@ import zen.misc
 from zen import logMsg, loadJson, getUsernameFromPublicKey
 
 
-# create the application instance 
+# create the application instance
 app = flask.Flask(__name__)
 app.config.update(
     # 300 seconds = 5 minutes lifetime session
-    PERMANENT_SESSION_LIFETIME = 300,
+    PERMANENT_SESSION_LIFETIME=300,
     # used to encrypt cookies
     # secret key is generated each time app is restarted
-    SECRET_KEY = os.urandom(24),
+    SECRET_KEY=os.urandom(24),
     # JS can't access cookies
-    SESSION_COOKIE_HTTPONLY = True,
+    SESSION_COOKIE_HTTPONLY=True,
     # bi use of https
-    SESSION_COOKIE_SECURE = False,
+    SESSION_COOKIE_SECURE=False,
     # update cookies on each request
     # cookie are outdated after PERMANENT_SESSION_LIFETIME seconds of idle
-    SESSION_REFRESH_EACH_REQUEST = True,
-    # 
-    TEMPLATES_AUTO_RELOAD = True
+    SESSION_REFRESH_EACH_REQUEST=True,
+    #
+    TEMPLATES_AUTO_RELOAD=True
 )
 
 
@@ -37,7 +37,7 @@ def check():
         data = json.loads(flask.request.data).get("data", False)
         logMsg("missing block :\n%s" % json.dumps(data, indent=2))
         zen.misc.notify("missed a block !\n%s" % json.dumps(data, indent=2))
-    return json.dumps({"zen-tbw::block/missed":True}, indent=2)
+    return json.dumps({"zen-tbw::block/missed": True}, indent=2)
 
 
 # compute true block weight
@@ -54,10 +54,12 @@ def spread():
             raise Exception("Error: can not reach username")
         # check autorization and exit if bad one
         webhook = loadJson("%s-webhook.json" % username)
-        if not webhook["token"].startswith(flask.request.headers["Authorization"]):
+        if not webhook["token"].startswith(
+            flask.request.headers["Authorization"]
+        ):
             raise Exception("Not autorized here")
         zen.tbw.TaskExecutioner.JOB.put([username, generatorPublicKey, block])
-    return json.dumps({"zen-tbw::block/forged":True})
+    return json.dumps({"zen-tbw::block/forged": True})
 
 
 @app.context_processor
@@ -67,25 +69,34 @@ def tweak():
     return dict(
         url_for=dated_url_for,
         tbw_config=tbw_config,
-        _currency=lambda value, fmt="r": flask.Markup(("%"+fmt+"&nbsp;%s") % (round(value,8), token)),
+        _currency=lambda value, fmt="r": flask.Markup(
+            ("%" + fmt + "&nbsp;%s") % (round(value, 8), token)
+        ),
         _dhm=lambda value: human_dhm(*dhm(value)),
         _address=lambda address: flask.Markup(
-            '<span class="not-ellipsed">%s</span><span class="ellipsed">%s</span>' % 
-            (address, "%s&nbsp;&#x2026;&nbsp;%s" % (address[:5],address[-5:])))
+            '<span class="not-ellipsed">%s</span>'
+            '<span class="ellipsed">%s</span>' %
+            (
+                address,
+                "%s&nbsp;&#x2026;&nbsp;%s" % (address[:5], address[-5:])
+            )
+        )
     )
 
 
 def dhm(last_blocks):
-    days = last_blocks * zen.rest.cfg.blocktime * zen.rest.cfg.activeDelegates / (3600.*24)
+    days = \
+        last_blocks * zen.rest.cfg.blocktime * \
+        zen.rest.cfg.activeDelegates / (3600.*24)
     hours = (days - math.floor(days)) * 24
     minutes = (hours - math.floor(hours)) * 60
     return math.floor(days), math.floor(hours), math.floor(minutes)
 
 
 def human_dhm(d, h, m):
-    return ("" if d<1 else ("%d day"%d + ("s " if d>1 else " "))) + \
-           ("" if h<1 else ("%d hour"%h + ("s " if h>1 else " "))) + \
-           ("" if m<1 else ("%d minute"%m + ("s" if m>1 else "")))
+    return ("" if d < 1 else ("%d day" % d + ("s " if d > 1 else " "))) + \
+           ("" if h < 1 else ("%d hour" % h + ("s " if h > 1 else " "))) + \
+           ("" if m < 1 else ("%d minute" % m + ("s" if m > 1 else "")))
 
 
 ########################
