3e6acac72d4777679732571aa242b1dddc131c85
Moustikitos <moustikitos@gmail.com>
2021-10-02 21:36:16 +0200

    back to normal

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,17 +1,9 @@
 # -*- coding:utf-8 -*-
-import os
 import zen
 import zen.biom
 
 
 def post_worker_init(worker):
-    for username in [
-        n.replace("-webhook.json", "") for n in next(os.walk(zen.JSON))[-1]
-        if n.endswith("-webhook.json")
-    ]:
-        zen.biom.getUsernameKeys(username)
-        zen.logMsg("%s secrets loaded and hidden !" % username)
-
     custom_peers = zen.loadJson("tbw.json").get("custom_peers", [])
     if len(custom_peers) > 0:
         zen.biom.dposlib.core.stop()
