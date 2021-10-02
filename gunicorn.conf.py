11eecfb7fd48fdb0552c04a65ce6e8f694110ed0
Moustikitos <moustikitos@gmail.com>
2021-10-02 18:11:28 +0200

    added secrets management

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,13 +1,21 @@
 # -*- coding:utf-8 -*-
+import os
 import zen
+import zen.biom
 
 
 def post_worker_init(worker):
-    # customize blockchain network
+    for username in [
+        n.replace("-webhook.json", "") for n in next(os.walk(zen.JSON))[-1]
+        if n.endswith("-webhook.json")
+    ]:
+        zen.biom.getUsernameKeys(username)
+        zen.logMsg("%s secrets loaded and hidden !" % username)
+
     custom_peers = zen.loadJson("tbw.json").get("custom_peers", [])
     if len(custom_peers) > 0:
-        zen.dposlib.core.stop()
-        zen.dposlib.rest.cfg.peers = custom_peers
+        zen.biom.dposlib.core.stop()
+        zen.biom.dposlib.rest.cfg.peers = custom_peers
 
 
 def on_exit(server):
